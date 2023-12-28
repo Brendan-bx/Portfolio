686d695bbaa643f3960ec07c77e559322d5afa86
Brendan Bx <Brendanbx@gmail.com>
2023-12-28 01:02:11 +0100

    page about

--- personal-portfolio/src/components/about/About.jsx
+++ personal-portfolio/src/components/about/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './About.css'
+import me from '../../assets/me-about.jpg'
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={me} alt='me'/>
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experiences</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>100% Satisfied</small>
+            </article><article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+          <p>Student in IT School, trying to learn many dev languages for myself or futures working projects (currently ReactJS)</p>
+        </div>
+        <a href='#contact' className='btn'>Contact me</a>
+      </div>
+    </section>
   )
 }
 
